8fbba79ce10132215aa2c024c72765a28e05b1f0
Natalie Lockyer <Natalie_lockyer@outlook.com>
2023-09-27 15:06:33 +0000

    Working in JS - Flipcard function added

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+//Homepage//
+const cards = document.querySelectorAll('.memory-card');
+
+
+//This is the gam logic, in order to flip the cards, check for a match, disable cards,//
+// unflip cards, reset board and shuffle.//
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+//flipcard function//
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+    this.classList.add('flip');
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+    secondCard = this;
+    lockBoard = true;
+
+    checkForMatch();
+}
+
