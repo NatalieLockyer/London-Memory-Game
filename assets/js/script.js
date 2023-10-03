455613e19c29aa9cf3ca50f7a115d3049f8855df
Natalie Lockyer <Natalie_lockyer@outlook.com>
2023-10-03 17:45:56 +0000

    Readded winner Modal as GitPod shut down lost

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+});
+
 //Homepage//
+//getters
 const cards = document.querySelectorAll('.memory-card');
+const resetGameBtn = document.getElementById('btn-reset-game');
+const movesCounter = document.getElementById('moves-counter');
+const modal = document.getElementById('myModal');
+const btn = document.getElementById('btn-how-to-play');
+const span = document.getElementsByClassName('close')[0];
+const timeValue = document.getElementById('timer-area');
+const modalWin = document.getElementById('winner-modal')
+
+
+//event listeners
+resetGameBtn.addEventListener('click', resetGame);
 
 //This is the game logic, in order to flip the cards, check for a match, disable cards,//
 // unflip cards, reset board and shuffle.//
@@ -7,19 +22,35 @@ const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+
+//Initial moves taken and Win count 
 let movesTaken = 0;
+let winCount = 0;
+let match = 0
+let length = movesTaken.length;
 
-//getters
-let resetGameBtn = document.getElementById('btn-reset-game');
-let movesCounter = document.getElementById('moves-counter');
-let modal = document.getElementById('myModal');
-let btn = document.getElementById('btn-how-to-play');
-let span = document.getElementsByClassName('close')[0];
 
-//event listeners
-resetGameBtn.addEventListener('click', resetGame);
+//Initial Time
+let seconds = 0;
+let minutes = 0;
+let hasStartedTimer = false;
+let interval = 0;
 
-//How to play function - When user clicks "how to play button - credit to w3schools for help with this code - details in ReadME
+
+const items = [
+    { name: "Big Ben", image: "front-card.jpg" },
+    { name: "Buckingham Palace", image: "buckingham-palace.jpg" },
+    { name: "Gerkin", image: "gerkin.jpg" },
+    { name: "London Eye", image: "london-eye.jpg" },
+    { name: "London Underground", image: "london-underground.jpg" },
+    { name: "Red Bus", image: "red-bus.jpg" },
+    { name: "The Shard", image: "the-shard.jpg" },
+    { name: "Tower Bridge", image: "tower-bridge.jpg" }
+]
+
+/*Modal to appear when "how to play button" is pressed
+Credit to w3schools for help with this code - details in ReadME*/
+
 btn.onclick = function () {
     modal.style.display = "block";
 };
@@ -29,24 +60,46 @@ span.onclick = function () {
     modal.style.display = "none";
 };
 
-//Countdown timer
-let timeLeft = 90;
-let countdownTimer = document.getElementById('timer-area');
+//Layout of Timer
+const timeGenerator = () => {
+    seconds += 1;
+    if (seconds > 60) {
+        minutes += 1;
+        seconds = 0;
+    }
 
-let timer = setInterval(countdown, 1000);
+    //Format of time before displaying
+    let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+    let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+    timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
 
-function countdown() {
-    if (timeLeft == -1) {
-        clearTimeout(timer);
-        doSomething();
-    } else {
-        countdownTimer.innerHTML = timeLeft + 'seconds remaining';
-        timeLeft--;
+};
+
+// Start the timer when first pair is turned over
+function startTimer() {
+    if (!hasStartedTimer) {
+        interval = setInterval(timeGenerator, 1000);
+        hasStartedTimer = true; // Set it to true so it doesn't start again.
     }
 }
 
+// flipcard function//
+function flipCardStart() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
 
+    this.classList.add('flip');
 
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    lockBoard = true;
+    checkForMatch();
+};
 
 //flipcard function//
 function flipCard() {
@@ -58,6 +111,10 @@ function flipCard() {
         firstCard = this;
         return;
     }
+
+    // Start the timer 
+    startTimer();
+
     secondCard = this;
     lockBoard = true;
     checkForMatch();
@@ -71,7 +128,7 @@ function checkForMatch() {
     console.log('moves taken:', movesTaken);
     console.log('moves counter:', movesCounter);
     movesCounter.innerText = movesTaken;
-};
+}
 
 //Disable card function 
 function disableCards() {
@@ -93,7 +150,10 @@ function unflipCards() {
 function resetBoard() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
-};
+    if (match == (items.length / 2)) {
+        window.alert("Congratulations! You Won!");
+    };
+}
 
 //Reset board btn function
 function resetGame() {
